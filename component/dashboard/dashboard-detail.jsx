36236f01b863f7501e5039534997d4c3546aa53d
Saad shahzad <saadshahz369@gmail.com>
2025-02-18 20:29:57 +0500

    issue resolved

--- component/dashboard/dashboard-detail.jsx
+++ component/dashboard/dashboard-detail.jsx
@@ -23,7 +23,8 @@ import {
   Modal,
   Radio,
   Row,
-  Upload, notification
+  Upload,
+  notification,
 } from "antd";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
@@ -31,18 +32,17 @@ import { useEffect, useState } from "react";
 const { TextArea } = Input;
 
 export default function DashboardDetail() {
-
   const [tasks, setTasks] = useState();
   const { data, status } = useSession();
   const [api, contextHolder] = notification.useNotification();
-  const [isloading, setIsloading] = useState(false)
-  const [userDate, setUserDate] = useState(data.user)
+  const [isloading, setIsloading] = useState(false);
+  const [userDate, setUserDate] = useState(data.user);
   const [isOpenModal, setIsOpenModal] = useState(false);
-  const [startDate, setStartDate] = useState()
-  const [endDate, setEndDate] = useState()
-  const [completedTask, setCompletedTask] = useState([])
-  const [notStartedTask, setNotStartedTask] = useState([])
-  const [inProgressTask, setInProgressTask] = useState([])
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+  const [completedTask, setCompletedTask] = useState([]);
+  const [notStartedTask, setNotStartedTask] = useState([]);
+  const [inProgressTask, setInProgressTask] = useState([]);
   const [fileList, setFileList] = useState([]);
 
   const [form] = Form.useForm();
@@ -60,16 +60,16 @@ export default function DashboardDetail() {
       userId: userDate.user_id,
       name: formData?.name,
       priority: formData?.priority,
-      status: 'notStarted',
+      status: "notStarted",
       image: null,
       description: formData?.description,
       start_date: startDate,
-      end_date: endDate
-    }
+      end_date: endDate,
+    };
 
     const response = await addNewTask(data, userDate.accessToken);
     if (response.success) {
-      getData()
+      getData();
       setIsOpenModal(false);
       setIsloading(false);
       openNotificationWithIcon("success", `Add new Task Successfully`);
@@ -80,21 +80,20 @@ export default function DashboardDetail() {
 
       openNotificationWithIcon("error", `${response.error}`);
     }
-
   };
   useEffect(() => {
     getData();
   }, []);
 
   const getData = async () => {
-
     const TaskData = await getTask(userDate.user_id, userDate.accessToken);
     setTasks(TaskData);
-
-    setCompletedTask(TaskData.filter((item) => item.status == 'completed'))
-    setNotStartedTask(TaskData.filter((item) => item.status == 'notStarted'))
-    setInProgressTask(TaskData.filter((item) => item.status == 'inProgress'))
-  }
+    if (TaskData.length > 0) {
+      setCompletedTask(TaskData.filter((item) => item.status == "completed"));
+      setNotStartedTask(TaskData.filter((item) => item.status == "notStarted"));
+      setInProgressTask(TaskData.filter((item) => item.status == "inProgress"));
+    }
+  };
   const openNotificationWithIcon = (type, title) => {
     api[type]({
       duration: "2",
@@ -118,6 +117,11 @@ export default function DashboardDetail() {
         ),
     });
   };
+
+  const date = new Date();
+  const today = `${date.getFullYear()}-0${date.getMonth() + 1}-${date.getDate()}`;
+  console.log("Date :", today);
+
 
   return (
     <section className="">
@@ -148,21 +152,26 @@ export default function DashboardDetail() {
               gap="small"
               className="tasklisting overflow-y-scroll	h-[400px] py-2"
             >
-              {tasks != undefined ? (tasks.length > 0 ? tasks.map((item, index) => {
-                return (
-                  <div
-                    className="xl:[48%] lg:w-[100%%] md:[100%] "
-                    key={index}
-                  >
-                    <TaskCard tasks={item} />
-                  </div>
-                );
-              }) : <p className="text-center font-semibold" >No Task Register</p>) :
-
-                <LoadingOutlined style={{ fontSize: "3rem", fill: "var(--color-primary)" }} />
-
-
-              }
+              {tasks != undefined ? (
+                tasks.length > 0 ? (
+                  tasks.map((item, index) => {
+                    return (
+                      <div
+                        className="xl:[48%] lg:w-[100%%] md:[100%] "
+                        key={index}
+                      >
+                        <TaskCard tasks={item} />
+                      </div>
+                    );
+                  })
+                ) : (
+                  <p className="text-center font-semibold">No Task Register</p>
+                )
+              ) : (
+                <LoadingOutlined
+                  style={{ fontSize: "3rem", fill: "var(--color-primary)" }}
+                />
+              )}
             </Flex>
           </div>
         </Col>
@@ -199,20 +208,28 @@ export default function DashboardDetail() {
                 gap="small"
                 className="tasklisting overflow-y-scroll	h-[200px] py-2"
               >
-
-                {completedTask != undefined ? (completedTask.length > 0 ? completedTask.map((item, index) => {
-                  return (
-                    <div
-                      className="xl:[48%] lg:w-[100%%] md:[100%] "
-                      key={index}
-                    >
-                      <TaskCard tasks={item} />
-                    </div>
-                  );
-                }) : <p className="text-center font-semibold" >No Task Register</p>) :
-                  <LoadingOutlined style={{ fontSize: "3rem", fill: "var(--color-primary)" }} />
-                }
-
+                {completedTask != undefined ? (
+                  completedTask.length > 0 ? (
+                    completedTask.map((item, index) => {
+                      return (
+                        <div
+                          className="xl:[48%] lg:w-[100%%] md:[100%] "
+                          key={index}
+                        >
+                          <TaskCard tasks={item} />
+                        </div>
+                      );
+                    })
+                  ) : (
+                    <p className="text-center font-semibold">
+                      No Task Register
+                    </p>
+                  )
+                ) : (
+                  <LoadingOutlined
+                    style={{ fontSize: "3rem", fill: "var(--color-primary)" }}
+                  />
+                )}
               </Flex>
             </div>
           </div>
@@ -264,9 +281,14 @@ export default function DashboardDetail() {
                       { required: true, message: "Please Enter Start Date" },
                     ]}
                   >
-                    <DatePicker className="w-full" onChange={(date, dateString) => {
-                      setStartDate(dateString)
-                    }} />
+                    <DatePicker
+                      className="w-full"
+                      minDate={'2025-02-17'} // dayjs("2019-08-01", dateFormat)
+                      format="YYYY-MM-DD"
+                      onChange={(date, dateString) => {
+                        setStartDate(dateString);
+                      }}
+                    />
                   </Form.Item>
                 </Col>
                 <Col span={12} className="pr-4">
@@ -282,9 +304,14 @@ export default function DashboardDetail() {
                       { required: true, message: "Please Enter End Date" },
                     ]}
                   >
-                    <DatePicker className="w-full" onChange={(date, dateString, event) => {
-                      setEndDate(dateString)
-                    }} />
+                    <DatePicker
+                      className="w-full"
+                      minDate={'2025-02-17'}
+                      format="YYYY-MM-DD"
+                      onChange={(date, dateString, event) => {
+                        setEndDate(dateString);
+                      }}
+                    />
                   </Form.Item>
                 </Col>
                 <Col span={12} className="pr-4">
@@ -297,9 +324,9 @@ export default function DashboardDetail() {
                     className="mt-3 mb-2 w-full"
                     style={{ fontSize: "16px", fontWeight: "600" }}
                     validateTrigger="onSubmit"
-                  // rules={[
-                  //   { required: true, message: "Please Upload Picture" },
-                  // ]}
+                    // rules={[
+                    //   { required: true, message: "Please Upload Picture" },
+                    // ]}
                   >
                     <Upload
                       action="https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload"
